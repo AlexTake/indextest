a76f829bb0542553f1b963d5015bb954be3b9975
Alex Chernyak <sashachernyak15@gmail.com>
2022-04-20 16:26:46 +0300

    123

--- indexsite.js
+++ indexsite.js
@@ -3,7 +3,6 @@ const button = document.querySelector('button');
 button.addEventListener('click', event => {
     document.getElementById('id1').style.color = window.Telegram.WebApp.themeParams.bgColor;
 });
-const button = document.querySelector('button');
 
 button.addEventListener('click', event => {
     document.getElementById('id2').style.color = 'red';
