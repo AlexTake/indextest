3301607a599e9c3f4f67150b8daa5a340b6b277b
Alex Chernyak <sashachernyak15@gmail.com>
2022-04-20 16:22:13 +0300

    fix

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const button = document.querySelector('button');
 button.addEventListener('click', event => {
     document.getElementById('id1').style.color = window.Telegram.WebApp.themeParams.bgColor;
 });
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+    document.getElementById('id2').style.color = 'red';
+});
